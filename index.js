75fd94d8deb5ed037fc2faff2c079f55bbb2b683
Amedeo Alfonsi <amedeo.alfonsi@gmail.com>
2026-02-24 16:15:36 +0100

    Initial index.js implementation

---
+++ index.js
@@ -0,0 +1,26 @@
+// Returns the server response, should be shown to the user
+async function set_alarm(idOrMessage, desc) {
+    const response = await fetch(
+        "set_alarm.php",
+        {
+            method: 'POST',
+            body: new URLSearchParams(
+                (typeof idOrMessage == 'string')
+                    ? { id: 4 /* allarme generico */, message: idOrMessage, desc }
+                    : { id: idOrMessage, desc }
+            )
+        }
+    );
+    const json = await response.json();
+    return json.message;
+}
+
+// Returns the emergency, state
+// Should contain
+// "STATO": 0 se tutto apposto, 1 se in emergenza
+// "MESSAGGIO": il testo principale dell'allarme
+// "DESCRIZIONE": la descrizione dell'allarme (il testo secondario)
+async function fetch_alarm() {
+    const response = await fetch("requestSchoolStateJs.php");
+    return await response.json();
+}
