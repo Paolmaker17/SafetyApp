43348362563aee60390adb39289d1f81676c6404
Paolmaker17 <paolo95ita@gmail.com>
2026-02-26 16:11:44 +0100

    Tabs Change

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+const buttons = [
+    document.querySelector("[data-name='invioAllarme']"),
+    document.querySelector("[data-name='allarmi']"),
+    document.querySelector("[data-name='utenti']")
+]
+const pages = [
+    invioAllarme,
+    allarmi,
+    utenti
+]
+
+for (const b of buttons) {
+    b.onclick = e => {
+        buttons.forEach(b => b.disabled = false)
+        b.disabled = true
+
+        for(const p of pages) {
+            p.style.display = p.id == b.dataset.name ? 'flex' : 'none'
+        }
+    }
+}
+
 // Returns the server response, should be shown to the user
 async function set_alarm(idOrMessage, desc) {
     const response = await fetch(
