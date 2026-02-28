cfa3a525396be4504708e21d48085c4272e37dbe
amedeo <amedeo.alfonsi@yahoo.com>
2026-02-28 20:08:26 +0100

    Add current emergency indicator, add semicolons

--- index.js
+++ index.js
@@ -1,82 +1,79 @@
-// Getione Tabs 
+// Getione Tabs
 const buttons = [
-    document.querySelector("[data-name='invioAllarme']"),
-    document.querySelector("[data-name='allarmi']"),
-    document.querySelector("[data-name='utenti']")
-]
-const pages = [
-    invioAllarme,
-    allarmi,
-    utenti
-]
+  document.querySelector("[data-name='invioAllarme']"),
+  document.querySelector("[data-name='allarmi']"),
+  document.querySelector("[data-name='utenti']"),
+];
+const pages = [invioAllarme, allarmi, utenti];
 
 for (const b of buttons) {
-    b.onclick = e => {
-        buttons.forEach(b => b.disabled = false)
-        b.disabled = true
+  b.onclick = (e) => {
+    buttons.forEach((b) => (b.disabled = false));
+    b.disabled = true;
 
-        for(const p of pages) {
-            p.style.display = p.id == b.dataset.name ? 'flex' : 'none'
-        }
+    for (const p of pages) {
+      p.style.display = p.id == b.dataset.name ? "flex" : "none";
     }
+  };
 }
 
 // Getione Menù a Tendina Utente
-const wrapper = document.getElementById("menu")
-const dropdown = document.getElementById("dropdown")
-let timeout
+const wrapper = document.getElementById("menu");
+const dropdown = document.getElementById("dropdown");
+let timeout;
 
 wrapper.addEventListener("mouseenter", () => {
-    clearTimeout(timeout)
-    dropdown.classList.remove("opacity-0", "translate-y-2", "pointer-events-none")
-    dropdown.classList.add("opacity-100", "translate-y-0")
-})
+  clearTimeout(timeout);
+  dropdown.classList.remove(
+    "opacity-0",
+    "translate-y-2",
+    "pointer-events-none",
+  );
+  dropdown.classList.add("opacity-100", "translate-y-0");
+});
 
 wrapper.addEventListener("mouseleave", () => {
-    timeout = setTimeout(() => {
-        dropdown.classList.add("opacity-0", "translate-y-2", "pointer-events-none")
-        dropdown.classList.remove("opacity-100", "translate-y-0")
-    }, 50)
-})
+  timeout = setTimeout(() => {
+    dropdown.classList.add("opacity-0", "translate-y-2", "pointer-events-none");
+    dropdown.classList.remove("opacity-100", "translate-y-0");
+  }, 50);
+});
 
 // Gestione Tema
-const toggle = document.getElementById("toggle")
+const toggle = document.getElementById("toggle");
 const applyTheme = (theme) => {
-    if (theme === "dark") {
-        document.documentElement.classList.add("dark")
-        toggle.checked = true
-    } else {
-        document.documentElement.classList.remove("dark")
-        toggle.checked = false
-    }
-    localStorage.setItem("theme", theme)
-}
+  if (theme === "dark") {
+    document.documentElement.classList.add("dark");
+    toggle.checked = true;
+  } else {
+    document.documentElement.classList.remove("dark");
+    toggle.checked = false;
+  }
+  localStorage.setItem("theme", theme);
+};
 
 toggle.addEventListener("change", () => {
-    const theme = toggle.checked ? "dark" : "light"
-    applyTheme(theme)
-})
+  const theme = toggle.checked ? "dark" : "light";
+  applyTheme(theme);
+});
 
 window.addEventListener("DOMContentLoaded", () => {
-    const savedTheme = localStorage.getItem("theme") || "light"
-    applyTheme(savedTheme)
-})
+  const savedTheme = localStorage.getItem("theme") || "light";
+  applyTheme(savedTheme);
+});
 
 // Returns the server response, should be shown to the user
 async function set_alarm(idOrMessage, desc) {
-    const response = await fetch(
-        "set_alarm.php",
-        {
-            method: 'POST',
-            body: new URLSearchParams(
-                (typeof idOrMessage == 'string')
-                    ? { id: 4 /* allarme generico */, message: idOrMessage, desc }
-                    : { id: idOrMessage, desc }
-            )
-        }
-    );
-    const json = await response.json();
-    return json.message;
+  const response = await fetch("set_alarm.php", {
+    method: "POST",
+    body: new URLSearchParams(
+      typeof idOrMessage == "string"
+        ? { id: 4 /* allarme generico */, message: idOrMessage, desc }
+        : { id: idOrMessage, desc },
+    ),
+  });
+  const json = await response.json();
+  return json.message;
 }
 
 // Returns the emergency, state
@@ -85,6 +82,30 @@ async function set_alarm(idOrMessage, desc) {
 // "MESSAGGIO": il testo principale dell'allarme
 // "DESCRIZIONE": la descrizione dell'allarme (il testo secondario)
 async function fetch_alarm() {
-    const response = await fetch("requestSchoolStateJs.php");
-    return await response.json();
+  const response = await fetch("requestSchoolStateJs.php");
+  if (!response.ok)
+    throw `Errore HTTP: ${response.status} ${response.statusText}`;
+  return await response.json();
 }
+
+async function updateIndicator() {
+  try {
+    const respose = await fetch_alarm();
+    if (response.STATO == 0) {
+      statusDot.style.background = "green";
+      statusMessage.textContent = "SafetyApp è in funzione";
+      statusDesc.textContent = "Nessuna emergenza in corso";
+    } else {
+      statusDot.style.background = "yellow";
+      statusMessage.textContent = response.MESSAGE;
+      statusDesc.textContent = response.DESCRIZIONE;
+    }
+  } catch (e) {
+    statusDot.style.background = "red";
+    statusMessage.textContent = "Errore del server";
+    statusDesc.textContent = e;
+  }
+}
+
+setInterval(updateIndicator, 1000);
+
