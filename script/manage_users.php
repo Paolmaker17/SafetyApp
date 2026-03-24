62df98c3ee9ceb8bd8d766f6aece86c6d6d7af1f
Amedeo Alfonsi <amedeo.alfonsi@gmail.com>
2026-03-24 16:27:29 +0100

    Fix 3

--- script/manage_users.php
+++ script/manage_users.php
@@ -17,7 +17,7 @@ try {
         case 'GET':
             header('Content-Type: application/json');
             $result = $conn->query("SELECT username FROM utenti");
-            echo json_encode($result->fetch_array(MYSQLI_ASSOC));
+            echo json_encode($result->fetch_all(MYSQLI_ASSOC));
         case 'PATCH':
         case 'PUT':
             $req = json_decode(file_get_contents('php://input'), true);
