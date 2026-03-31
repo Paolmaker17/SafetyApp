3a19adc7d3f35cfa1942a07335702c7a0f234bda
Paolmaker17 <paolo95ita@gmail.com>
2026-03-31 15:29:12 +0200

    Ui Change 9

--- index.js
+++ index.js
@@ -262,7 +262,7 @@ async function usersList(ev) {
 
   usersTable.append(
     ...response.map(user =>
-      elClass("div", "border-b border-t border-(--border) transition flex py-2",
+      elClass("div", "border-b border-t border-(--border) transition flex h-8 items-center",
         elClass("span", "font-semibold flex-1",
           user
         ),
