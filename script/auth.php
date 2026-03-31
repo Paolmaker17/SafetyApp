b9c56e37a5b65f0ba69144fea62008f3ae878d8c
Paolmaker17 <paolo95ita@gmail.com>
2026-03-31 16:05:29 +0200

    Header Auth.php

--- script/auth.php
+++ script/auth.php
@@ -46,7 +46,7 @@ try {
     $_SESSION["autenticato"] = true;
     $_SESSION["username"] = $username;
     $_SESSION["time"] = time();
-    header(header: 'Location: index.php');
+    header('Location: index.php');
     exit(200);
 } catch (Exception $e) {
     echo $e->getMessage();
