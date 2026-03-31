e4d99a1796a8a7f221601ca86d917d01c2e2e608
Paolmaker17 <paolo95ita@gmail.com>
2026-03-31 16:17:19 +0200

    Access-Control-Expose-Headers

--- login.js
+++ login.js
@@ -68,11 +68,12 @@ form.onsubmit = async (ev) => {
 
   const response = await fetch("auth.php", {
     method: "POST",
+    headers: {'Access-Control-Expose-Headers': 'Location'},
     redirect: 'manual',
     body: new FormData(form)
   })
 
-  const location = response.headers.get('Location');
+  const location = response.redirect;
   if(response.status == 200 && location){
     window.location.assign(location);
     return;
