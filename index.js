27283e27ee4e4bf2f64fb4f05e06fb13fe5f72f4
Paolmaker17 <paolo95ita@gmail.com>
2026-03-31 15:25:05 +0200

    Ui Change 6

--- index.js
+++ index.js
@@ -262,31 +262,27 @@ async function usersList(ev) {
 
   usersTable.append(
     ...response.map(user =>
-      elClass("tr", "border-b border-t border-(--border) transition flex flex-wrap",
-        elClass("td", "p-3 flex items-center gap-3",
+      elClass("div", "border-b border-t border-(--border) transition flex flex-wrap",
           elClass("span", "font-semibold",
             user
           ),
         ),
-        elClass("td", "p-3 relative right-0",
-          elProps("span",
-            {
-              classList: "px-2 py-1 text-xs rounded-full bg-red-500/10 text-red-500",
-              onclick: async () => {
-                const response = await fetch("manage_users.php?" + new URLSearchParams({ username: user }),
-                  { method: "DELETE" }
-                ).then(it => it.text());
-                
-                showToast(response);
-                usersList();
-              }
-            },
-            "Remove"
-          ),
+        elProps("span",
+          {
+            classList: "px-2 py-1 text-xs rounded-full bg-red-500/10 text-red-500",
+            onclick: async () => {
+              const response = await fetch("manage_users.php?" + new URLSearchParams({ username: user }),
+                { method: "DELETE" }
+              ).then(it => it.text());
+              
+              showToast(response);
+              usersList();
+            }
+          },
+          "Remove"
         ),
-      )
+      ),
     )
-  )
 }
 
 usersList();
